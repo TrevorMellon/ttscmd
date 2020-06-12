095b1f3aa3fb0b702a2f5053f007e69ce5806e2b
Codified <codified@protonmail.com>
2020-06-12 12:01:33 +0000

    Update req.cmake

--- cmake/req.cmake
+++ cmake/req.cmake
@@ -1,11 +1,12 @@
 SET(Boost_USE_STATIC_LIBS   OFF)
-SET(Boost_USE_MULTITHREADED OFF)
+#SET(Boost_USE_MULTITHREADED OFF)
 
 find_package(Boost COMPONENTS program_options locale regex system thread filesystem)
 
 find_package(ICU OPTIONAL_COMPONENTS uc data i18n)
 
 include_directories(${Boost_INCLUDE_DIRS})
+add_definitions(-DBOOST_ALL_NO_LIB=1 BOOST_ALL_DYN_LINK=1)
 add_definitions(-DUSING_BOOST=1)
 include_directories(${ICU_INCLUDE_DIRS})
 
