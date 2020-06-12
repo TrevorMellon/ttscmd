8071e76760b8e3f35c39af707fd902fc39850b05
Trevor Mellon <codified@protonmail.com>
2020-06-12 14:37:39 +0100

    tweak licence header

--- include/ttscmd/Unicode.h
+++ include/ttscmd/Unicode.h
@@ -1,7 +1,6 @@
 /*****************************************************
 *                                                    *
 *  Copyright (c) 2016 Trevor Mellon                  *
-*  For further information contact t@jno.io          *
 *                                                    *
 *  Licensed under the BSD 3-clause license           *
 *  See LICENCE file for more information             *
--- src/Voices.cpp
+++ src/Voices.cpp
@@ -1,7 +1,6 @@
 /*****************************************************
 *                                                    *
 *  copyright (c) 2018 Trevor Mellon                  *
-*  For further information contact t@jno.io          *
 *                                                    *
 *  Licensed under the BSD 3-clause license           *
 *  See LICENCE file for more information             *
